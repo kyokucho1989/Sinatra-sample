dcaa4a23d08e6ab1427bcfd41c1ad64568bc102d
ko <kyokucho1989@gmail.com>
2023-09-01 05:19:56 +0900

    定数private化と名称変更

--- models/memo.rb
+++ models/memo.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Memoのモデルクラス
 class Memo
   MEMO_SAVE_FILE = '.memo_data'
+  private_constant :MEMO_SAVE_FILE
   attr_reader :id, :title, :content
 
   def initialize(**params)
@@ -14,31 +14,31 @@ class Memo
     memo_data = JSON.parse(File.read(MEMO_SAVE_FILE), symbolize_names: true)
     @id = memo_data[:last_id] + 1
     memo_data[:last_id] = id
-    memo_data[:memo_lists] << to_h
+    memo_data[:memo_list] << to_h
     File.open(MEMO_SAVE_FILE, 'w') { |file| file.write(JSON.dump(memo_data)) }
   end
 
   class << self
     def all
       unless File.exist?(MEMO_SAVE_FILE)
-        memo_data = { last_id: 0, memo_lists: [] }
+        memo_data = { last_id: 0, memo_list: [] }
         File.open(MEMO_SAVE_FILE, 'w') { |file| file.write(JSON.dump(memo_data)) }
       end
       memo_data = JSON.parse(File.read(MEMO_SAVE_FILE), symbolize_names: true)
-      memo_data[:memo_lists]
+      memo_data[:memo_list]
     end
 
     def find(id)
       memo_data = JSON.parse(File.read(MEMO_SAVE_FILE), symbolize_names: true)
-      memo_data[:memo_lists].find { |memo| memo[:id] == id.to_i }
+      memo_data[:memo_list].find { |memo| memo[:id] == id.to_i }
     end
 
     def update(**params)
       memo_data = JSON.parse(File.read(MEMO_SAVE_FILE), symbolize_names: true)
-      index = memo_data[:memo_lists].index do |memo|
+      index = memo_data[:memo_list].index do |memo|
         memo[:id] == params['id'].to_i
       end
-      data_to_update = memo_data[:memo_lists][index]
+      data_to_update = memo_data[:memo_list][index]
       data_to_update[:title] = params['title']
       data_to_update[:content] = params['content']
       File.open(MEMO_SAVE_FILE, 'w') { |file| file.write(JSON.dump(memo_data)) }
@@ -46,7 +46,7 @@ class Memo
 
     def delete(id)
       memo_data = JSON.parse(File.read(MEMO_SAVE_FILE), symbolize_names: true)
-      memo_data[:memo_lists].delete_if { |memo| memo[:id] == id.to_i }
+      memo_data[:memo_list].delete_if { |memo| memo[:id] == id.to_i }
       File.open(MEMO_SAVE_FILE, 'w') { |file| file.write(JSON.dump(memo_data)) }
     end
   end
