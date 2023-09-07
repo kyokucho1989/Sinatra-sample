96fb8b17741a35c9d68b94084921358bf5bfa4ea
ko <kyokucho1989@gmail.com>
2023-09-08 05:47:12 +0900

    rubocop 実施とdb名 定数指定

--- models/memo.rb
+++ models/memo.rb
@@ -2,10 +2,9 @@
 
 require 'pg'
 
-
 class Memo
-  MEMO_SAVE_FILE = '.memo_data'
-  private_constant :MEMO_SAVE_FILE
+  DB_NAME = 'sinatra-db'
+  private_constant :DB_NAME
   attr_reader :id, :title, :content
 
   def initialize(**params)
@@ -14,32 +13,32 @@ class Memo
   end
 
   def save
-    conn = PG.connect( dbname: 'sinatra-db' )
-    conn.exec_params( "INSERT INTO memotable VALUES (nextval('id'), $1, $2)",[@title, @content] )
+    conn = PG.connect(dbname: DB_NAME)
+    conn.exec_params("INSERT INTO memotable VALUES (nextval('id'), $1, $2)", [@title, @content])
     memo_data = conn.exec('SELECT lastval()')
     @id = memo_data.first['lastval'].to_i
   end
 
   class << self
     def all
-      conn = PG.connect( dbname: 'sinatra-db' )
-      memo_data = conn.exec( "SELECT * FROM memotable ORDER BY id" )
+      conn = PG.connect(dbname: DB_NAME)
+      conn.exec('SELECT * FROM memotable ORDER BY id')
     end
 
     def find(id)
-      conn = PG.connect( dbname: 'sinatra-db' )
-      memo_data = conn.exec_params( "SELECT * FROM memotable WHERE id = $1", [id] )
+      conn = PG.connect(dbname: DB_NAME)
+      memo_data = conn.exec_params('SELECT * FROM memotable WHERE id = $1', [id])
       memo_data.first
     end
 
     def update(**params)
-      conn = PG.connect( dbname: 'sinatra-db' )
-      conn.exec_params( "UPDATE memotable SET title = $1, content = $2 WHERE id = $3" ,[params['title'], params['content'], params['id']])
+      conn = PG.connect(dbname: DB_NAME)
+      conn.exec_params('UPDATE memotable SET title = $1, content = $2 WHERE id = $3', [params['title'], params['content'], params['id']])
     end
 
     def delete(id)
-      conn = PG.connect( dbname: 'sinatra-db' )
-      conn.exec( "DELETE FROM memotable WHERE id = #{id}" )
+      conn = PG.connect(dbname: DB_NAME)
+      conn.exec("DELETE FROM memotable WHERE id = #{id}")
     end
   end
 
