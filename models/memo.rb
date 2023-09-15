abe0b1de1538bde1a461b55d32cb8e3627ce271c
ko <kyokucho1989@gmail.com>
2023-09-16 07:59:11 +0900

    connをトップレベルの定数に変更

--- models/memo.rb
+++ models/memo.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 require 'pg'
+DB_NAME = 'sinatra-db'
+CONN = PG.connect(dbname: DB_NAME)
 
 class Memo
-  DB_NAME = 'sinatra-db'
-  private_constant :DB_NAME
-  @@conn = PG.connect(dbname: DB_NAME)
   attr_reader :id, :title, :content
 
   def initialize(**params)
@@ -15,14 +14,14 @@ class Memo
 
   def save
     save_memo = { title: @title, content: @content }
-    @@conn.exec_params("INSERT INTO memotable VALUES (nextval('id'), $1, $2)", [save_memo[:title], save_memo[:content]])
-    memo_data = @@conn.exec('SELECT lastval()')
+    CONN.exec_params("INSERT INTO memotable VALUES (nextval('id'), $1, $2)", [save_memo[:title], save_memo[:content]])
+    memo_data = CONN.exec('SELECT lastval()')
     @id = memo_data.first['lastval'].to_i
   end
 
   class << self
     def all
-      all_memo_data = @@conn.exec('SELECT * FROM memotable ORDER BY id')
+      all_memo_data = CONN.exec('SELECT * FROM memotable ORDER BY id')
       all_memo_data.map do |memo_data|
         memo_data.transform_keys!(&:to_sym)
         memo_data[:id] = memo_data[:id].to_i
@@ -31,18 +30,18 @@ class Memo
     end
 
     def find(id)
-      memo_data = @@conn.exec_params('SELECT * FROM memotable WHERE id = $1', [id])
+      memo_data = CONN.exec_params('SELECT * FROM memotable WHERE id = $1', [id])
       memo_data = memo_data.first.transform_keys!(&:to_sym)
       memo_data[:id] = memo_data[:id].to_i
       memo_data
     end
 
     def update(**params)
-      @@conn.exec_params('UPDATE memotable SET title = $1, content = $2 WHERE id = $3', [params['title'], params['content'], params['id']])
+      CONN.exec_params('UPDATE memotable SET title = $1, content = $2 WHERE id = $3', [params['title'], params['content'], params['id']])
     end
 
     def delete(id)
-      @@conn.exec('DELETE FROM memotable WHERE id = $1', [id])
+      CONN.exec('DELETE FROM memotable WHERE id = $1', [id])
     end
   end
 
